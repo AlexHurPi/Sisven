27061a02397f092f93bc6f0064e4e441381c875a
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-11-15 11:22:51 -0500

    Index, controller y new de Customer

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+use Illuminate\Support\Facades\DB;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       //$categories = Category::all();      
+       $customers =DB::table('customers')       
+       ->select('customers.*')
+       ->get();
+       return view("customers.index",['customers' => $customers]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customers extends Model
+class Customer extends Model
 {
     use HasFactory;
-    protected $table = 'tb_customers';
+    protected $table = 'customers';
     protected $fillable = [
         'document_number',
         'first_name',
