05752cd154240015c318c7d5a6e34e0f3be8d527
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-11-18 21:53:20 -0500

    products model

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -5,15 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Products extends Model{
-  
+class Products extends Model
+{  
     use HasFactory;
-    protected $table = "tb_products";
-    protected $primarykey = 'id'; 
-    //private $name;
-    //private $price;
-    //private $stock;
-    //private $dcategory_id;
+    protected $table = "products";    
+    protected $fillable = [
+        'name',
+        'price',
+        'stock',
+        'category_id',
+    ];
     public $timestamps = false;
-
 }
