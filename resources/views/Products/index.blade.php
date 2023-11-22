65703d3ec106aa0b465a4c3b7164b0b569aab429
Alexander Hurtado <alexander.hurtado7170@gmail.com>
2023-11-22 18:59:09 -0500

    index products

---
+++ resources/views/Products/index.blade.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="resources/css/estilos.css">
+
+    <title>Products List</title>
+  </head>
+  <body>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Products List') }}
+            </h2>
+        </x-slot>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <a href="{{ route('products.create') }}"
+                            class="btn btn-success"
+                            >Add</a>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Id Product</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col">Category Id</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($products as $product)
+                                <tr>
+                                    <th scope="row">{{ $product->id }}</th>
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ $product->price }}</td>
+                                    <td>{{ $product->stock }}</td>
+                                    <td>{{ $product->categoryname }}</td>
+                                    <td>
+                                        <a href="{{ route('products.edit', ['product' => $product->id]) }}"
+                                            class="btn btn-info">
+                                            Edit</a>
+                                            
+                                        <form
+                                            action="{{ route('products.destroy', ['product' => $product->id]) }}"
+                                            method='POST' style="display: inline-block">
+                                            @method('delete')
+                                            @csrf
+                                            <input
+                                                class="btn btn-danger"
+                                                type="submit" value="Delete">
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </x-app-layout>
+</body>
